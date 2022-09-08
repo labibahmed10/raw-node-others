65f11ac042564813282555186cf4b619bd07ce0e
labibahmed10 <labib.ahmed.372@gmail.com>
2022-09-08 21:20:35 +0600

    first commit

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const server = http.createServer(function (req, res) {
   res.end();
 });
 
+// basic node server with json response
+const server1 = http.createServer(function (req, res) {
+  res.writeHead(200, { "content-type": "application/json" });
+  res.write(JSON.stringify({ code: "my first raw node Alhamdulillah" }));
+  res.end();
+});
 
+const server3 = http.createServer(function (req, res) {
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<p>anyhing</p>"));
+  const hel = req.url;
+  const urla = "http://localhost:5000/comment?what=you&so=nothing";
+  console.log(url.parse(urla, true));
+  res.end();
+});
 
 server.listen(5000);
