376ded0d29ec2a560e7432bf313aa21344dea3b6
labibahmed10 <labib.ahmed.372@gmail.com>
2022-09-08 23:55:18 +0600

    writing in a file and reading it in asynchronous way

--- file.js
+++ file.js
@@ -1,14 +1,41 @@
 const http = require("http");
+const fs = require("fs");
+const e = require("cors");
 
 // we have to create a server here and to the function from module js, we have to create a server listen and "nodemon thisFile.js" otherwise it won't work.
 
-let any = require("./module");
-console.log(any);
+// let any = require("./module");
+// console.log(any);
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "content-type": "text/html" });
+//   res.write("<p>anyhing</p>");
+//   res.end();
+// });
+
+// server.listen(5000);
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<p>anyhing</p>");
-  res.end();
+  if (req.url == "/") {
+    
+    // writing in a file asynchronous way
+    fs.writeFile("data.txt", "Hijibijihijibiji", (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    // reading in file asynchronous way
+    fs.readFile("data.txt", (err, data) => {
+      if (err) {
+        res.write("There is a error");
+        res.end();
+      } else {
+        res.write(data);
+        res.end();
+      }
+    });
+  }
 });
 
 server.listen(5000);
